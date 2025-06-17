57303ed67d156029b48e2a77738884abf0063b4d
abdulrafaykhkan125 <45812@students.riphah.edu.pk>
2025-06-17 11:51:15 +0500

    final commit

--- script.js
+++ script.js
@@ -22,3 +22,35 @@ document.addEventListener("click", function (e) {
 }
 
 hamburger();
+
+function Animation(){
+    gsap.from(".nav",{
+        y:-100,
+        duration:1,
+        opacity:0,
+    })
+    gsap.from(".links>a",{
+        y:-100,
+        opacity:0,
+        duration:1,
+        stagger:.3,
+    })
+    gsap.from(".herol",{
+        x:-100,
+        duration:1,
+        opacity:0,
+    })
+    gsap.from(".heror>img",{
+        scale:1.2,
+        opacity:0,
+        duration:1.2,
+    })
+    gsap.from(".hehe",{
+        x:-50,
+        scale:.8,
+        opacity:0,
+        color:white,
+    })
+}
+
+Animation();
